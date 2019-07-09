effd59dd448e2a250c0c386dd46014307a81592a
ryerajana <ryerajana@sonicwall.com>
2019-07-09 03:58:43 -0700

    starting node of two list

---
+++ hackerRank/mergeNode.cpp
@@ -0,0 +1,166 @@
+
+/*
+ * Author: Rambabu Yerajana
+ * Text:   HackerRank 
+ * Topic:  Puzzle 
+ * Statement:  Find starting Node of two mergelist 
+ * Description:  Find starting node of two list 
+ * */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+  public:
+    int data;
+    SinglyLinkedListNode *next;
+
+    SinglyLinkedListNode(int node_data) {
+      this->data = node_data;
+      this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList {
+  public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
+
+    SinglyLinkedList() {
+      this->head = nullptr;
+      this->tail = nullptr;
+    }
+
+    void insert_node(int node_data) {
+      SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+      if (!this->head) {
+        this->head = node;
+      } else {
+        this->tail->next = node;
+      }
+
+      this->tail = node;
+    }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+  while (node) {
+    fout << node->data;
+
+    node = node->next;
+
+    if (node) {
+      fout << sep;
+    }
+  }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+  while (node) {
+    SinglyLinkedListNode* temp = node;
+    node = node->next;
+
+    free(temp);
+  }
+
+  int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    int count1 = 0;
+    int count2 = 0;
+    SinglyLinkedListNode *curr1=head1;
+    SinglyLinkedListNode *curr2=head2;
+    if(!head1 || !head2 ) return 0;
+    while(curr1->next){count1++;curr1=curr1->next;}
+    while(curr2->next){count2++;curr2=curr2->next;}
+    int diff = abs(count1-count2);
+    curr1=head1;
+    curr2=head2;
+    if(count1 > count2){
+      while(curr1->next && diff--){
+        curr1=curr1->next;
+      }
+    }
+    else{
+      while(curr2->next && diff--){
+        curr2=curr2->next;
+      }
+    }
+
+    while( curr1->next && curr2->next){
+      if(curr1 == curr2) break ;
+
+      curr1=curr1->next;
+      curr2=curr2->next;
+
+    }
+    return curr1->data;
+  }
+
+  int main()
+  {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+      int index;
+      cin >> index;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+      SinglyLinkedList* llist1 = new SinglyLinkedList();
+
+      int llist1_count;
+      cin >> llist1_count;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+      for (int i = 0; i < llist1_count; i++) {
+        int llist1_item;
+        cin >> llist1_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist1->insert_node(llist1_item);
+      }
+
+      SinglyLinkedList* llist2 = new SinglyLinkedList();
+
+      int llist2_count;
+      cin >> llist2_count;
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+      for (int i = 0; i < llist2_count; i++) {
+        int llist2_item;
+        cin >> llist2_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist2->insert_node(llist2_item);
+      }
+
+      SinglyLinkedListNode* ptr1 = llist1->head;
+      SinglyLinkedListNode* ptr2 = llist2->head;
+
+      for (int i = 0; i < llist1_count; i++) {
+        if (i < index) {
+          ptr1 = ptr1->next;
+        }
+      }
+
+      for (int i = 0; i < llist2_count; i++) {
+        if (i != llist2_count-1) {
+          ptr2 = ptr2->next;
+        }
+      }
+
+      ptr2->next = ptr1;
+
+      int result = findMergeNode(llist1->head, llist2->head);
+
+      fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+
