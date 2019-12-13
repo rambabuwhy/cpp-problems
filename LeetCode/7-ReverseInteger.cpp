6c0ffe6022d7584774f4a3e1a2a9d738ff1c09a0
Rambabu Yerajana <rambabuwhy@gmail.com>
2019-12-14 00:43:34 +0530

    Add files via upload

---
+++ LeetCode/7-ReverseInteger.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int result=0;
+
+
+        while(x){
+            int digit = x%10;
+             x /= 10;
+            if (result > INT_MAX/10 || (result == INT_MAX / 10 && digit > 7)) return 0;
+            if (result < INT_MIN/10 || (result == INT_MIN / 10 && digit < -8)) return 0;
+            result = result  * 10 +  digit;
+        }
+        return result;
+    }
+};
