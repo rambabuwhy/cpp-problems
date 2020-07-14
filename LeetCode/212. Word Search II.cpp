fab55e67b0130176fb189f3c2f6fc5dddddbe5d9
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-07-14 21:11:29 +0530

    Create 212. Word Search II.cpp

---
+++ LeetCode/212. Word Search II.cpp
@@ -0,0 +1,73 @@
+/*
+
+Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+*/
+class Solution {
+    
+    //trie data structure
+    struct TrieNode {
+        TrieNode *children[26];
+        string word;
+
+        TrieNode() : word("") {
+            for (int i = 0; i < 26; i++) {
+                children[i] = nullptr;
+            }
+        }
+    };
+    
+public:
+    
+    //1. construct trie  2.  do dfs
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        TrieNode *root = buildTrie(words);
+        vector<string> result;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                dfs(board, i, j, root, result);
+            }
+        }
+        return result;
+        
+    }
+    
+    //Trie
+    TrieNode *buildTrie(vector<string> &words) {
+        TrieNode *root = new TrieNode();
+        for (int j = 0; j < words.size(); j++) {
+            string word = words[j];
+            TrieNode *curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word[i] - 'a';
+                if (curr->children[c] == nullptr) {
+                    curr->children[c] = new TrieNode();
+                }
+                curr = curr->children[c];
+            }
+            curr->word = word;
+        }
+        return root;
+    }
+    
+    //dfs
+    void dfs(vector<vector<char>> &board, int i, int j, TrieNode *p, vector<string> &result) {
+        char c = board[i][j];
+        if (c == '#' || !p->children[c - 'a']) return;
+        p = p->children[c - 'a'];
+        if (p->word.size() > 0) {
+            result.push_back(p->word);
+            p->word = "";
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j, p, result);
+        if (j > 0) dfs(board, i, j - 1, p, result);
+        if (i < board.size() - 1) dfs(board, i + 1, j, p, result);
+        if (j < board[0].size() - 1) dfs(board, i, j + 1, p, result);
+        board[i][j] = c;
+    }
+private:
+    
+};
