7ecfa9b2680384e17b19b0e3402a377745af504e
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-07-28 14:28:48 +0530

    Update and rename 9-PalindromeNumber.cpp to 9. Palindrome Number.cpp

---
+++ LeetCode/9. Palindrome Number.cpp
@@ -0,0 +1,35 @@
+/*
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int result = 0;
+        int i=x;
+        while(i){
+            if(result > INT_MAX/10 || result == INT_MAX/10 && i%10 >7) return false;
+            result = result * 10 + i % 10;
+            i /= 10;
+        }
+        return result == x;
+
+    }
+};
