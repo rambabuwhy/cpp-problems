7f5e1a807bc6e3759342d1a16cb9f283d4026d1c
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-07-28 14:33:34 +0530

    Update and rename 16-3SumClosest.cpp to 16. 3Sum Closest.cpp

--- LeetCode/16. 3Sum Closest.cpp
+++ LeetCode/16. 3Sum Closest.cpp
@@ -1,3 +1,15 @@
+/*
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+*/
+
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
