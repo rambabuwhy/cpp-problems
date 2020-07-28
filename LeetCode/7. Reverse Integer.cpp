98fda506e7c5e77b94191f1f90bde7fbcf651d4c
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-07-28 14:28:07 +0530

    Update and rename 7-ReverseInteger.cpp to 7. Reverse Integer.cpp

--- LeetCode/7. Reverse Integer.cpp
+++ LeetCode/7. Reverse Integer.cpp
@@ -1,3 +1,21 @@
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+
+*/
+
 class Solution {
 public:
     int reverse(int x) {
