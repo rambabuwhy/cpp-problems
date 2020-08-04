9db27e42fa79fe8e378ce925bddc44302fa10bb5
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-08-04 20:40:52 +0530

    Create 338. Counting Bits.cpp

---
+++ LeetCode/338. Counting Bits.cpp
@@ -0,0 +1,30 @@
+/*
+  Counting Bits
+
+Solution
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example 1:
+
+Input: 2
+Output: [0,1,1]
+Example 2:
+
+Input: 5
+Output: [0,1,1,2,1,2]
+
+*/
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        
+        vector<int> result(num+1,0);
+        for(int i = 1; i <= num; i++){
+            result[i]=result[(i & (i-1))] +1;
+        }
+        
+        return result;
+        
+    }
+};
