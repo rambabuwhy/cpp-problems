bf27faac52e3c27c7c8cf64c4223bc72edfc3bc5
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-06 23:05:21 +0530

    Create 117. Populating Next Right Pointers in Each Node II.cpp

---
+++ LeetCode/117. Populating Next Right Pointers in Each Node II.cpp
@@ -0,0 +1,70 @@
+/*
+
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+        if(root == nullptr) return root;
+        
+        
+        queue<Node*>  Q;
+        
+        Q.push(root);
+        
+        while(!Q.empty()){
+            
+            int size = Q.size();
+            
+            for(int i = 0;  i< size; i++){
+                
+                Node* curr = Q.front();
+                Q.pop();
+                
+                if(i < size -1)  
+                curr->next = Q.front();
+                
+                if(curr->left) Q.push(curr->left);
+                if(curr->right)  Q.push(curr->right);
+                
+                
+            }
+            
+        }
+        
+        return root;
+        
+    }
+};
