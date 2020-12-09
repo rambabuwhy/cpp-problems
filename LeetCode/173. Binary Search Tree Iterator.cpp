31b77286a59fa93a38bc210c298e01fd7187fa44
Rambabu Yerajana <rambabuwhy@gmail.com>
2020-12-09 22:12:53 +0530

    Create 173. Binary Search Tree Iterator.cpp

---
+++ LeetCode/173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,69 @@
+/*
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        
+        idx = -1;
+        inorder(root);
+        
+    }
+    
+    int next() {
+        
+        return ns[++idx];
+        
+    }
+    
+    bool hasNext() {
+        
+        return  idx+1 < ns.size();
+        
+    }
+private:
+    vector<int> ns;
+    int idx;
+    
+    void inorder(TreeNode *root) {
+
+        if (root == nullptr) {
+            return;
+        }
+
+        inorder(root->left);
+        ns.push_back(root->val);
+        inorder(root->right);
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
