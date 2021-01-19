613dcf2d6d772567dbcc9db4fd00b02b26517249
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-01-19 10:22:39 +0530

    Create 1679. Max Number of K-Sum Pairs.cpp

---
+++ LeetCode/1679. Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,54 @@
+/*
+
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+
+*/
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        
+        //two pointer sort  Solution
+        if(nums.size()<2) return 0;
+        
+        sort(nums.begin(),nums.end());
+        
+        int left = 0;
+        int right = nums.size()-1;
+        
+        int count = 0;
+        while(left<right){
+            if(nums[left]+nums[right] < k)
+            left++;
+            else if(nums[left]+nums[right] > k)
+            right--;
+            else
+            left++,right--,count++;
+        }
+        
+        return count;
+        
+    }
+};
