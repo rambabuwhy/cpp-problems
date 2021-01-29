9f307a45749c967a1c816a43120245b6b8cce293
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-01-29 11:54:29 +0530

    Update 10. Regular Expression Matching.cpp

--- LeetCode/10. Regular Expression Matching.cpp
+++ LeetCode/10. Regular Expression Matching.cpp
@@ -50,15 +50,19 @@ Output: false
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        
         if(p.empty()) return s.empty();
+        
+        //step 1: first match
         bool firstMatch = ((!s.empty()) && (p[0] == s[0] || p[0] == '.'));
-        if(p.length() >=2 && p[1] == '*'){
-
-            return (isMatch(s,p.substr(2)) || firstMatch && isMatch(s.substr(1),p));
-
-        }
-        else {
-             return (firstMatch && isMatch(s.substr(1),p.substr(1)));
-        }
+        
+        //step 2: *  present
+        if(p.length() >=2 && p[1] == '*')
+        return (isMatch(s,p.substr(2)) || firstMatch && isMatch(s.substr(1),p));   
+        
+        //step 3: not a *
+        else 
+        return (firstMatch && isMatch(s.substr(1),p.substr(1)));
+        
     }
 };
