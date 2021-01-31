d2fec9e9ab25cc51f466cc7d2498b74228795769
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-01-31 21:39:14 +0530

    Update 344. Reverse String.cpp

--- LeetCode/344. Reverse String.cpp
+++ LeetCode/344. Reverse String.cpp
@@ -5,17 +5,18 @@ Do not allocate extra space for another array, you must do this by modifying the
 
 You may assume all the characters consist of printable ascii characters.
 */
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        
         if(s.empty()) return;
         if(s.size() == 1) return;
-        for(int i=0;i<s.size()/2;i++){
-            
+        
+        for(int i=0; i<s.size()/2; i++){
             char temp = s[i];
             s[i] = s[s.size()-1-i];
             s[s.size()-1-i]=temp;
-       
         }
         
     }
