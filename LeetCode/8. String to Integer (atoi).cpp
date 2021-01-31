d26d9c38932c8d0e98a50fbd169b104f393a6351
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-01-31 21:25:21 +0530

    Update 8. String to Integer (atoi).cpp

--- LeetCode/8. String to Integer (atoi).cpp
+++ LeetCode/8. String to Integer (atoi).cpp
@@ -15,12 +15,16 @@ If no valid conversion could be performed, a zero value is returned.
 class Solution {
 public:
     int myAtoi(string str) {
-     int result = 0;
+        int result = 0;
         int index = 0;
         bool neg = false;
+        
+        //remove space
         while(str[index]==' '){
             index++;
         }
+        
+        //store sign
         if(str[index]=='-'){
             neg=true;
             index++;
@@ -32,18 +36,22 @@ public:
         else if(!isdigit(str[index])){
             return 0;
         }
+        
+        //logic
         while(isdigit(str[index])){
-            int digit = str[index++]-'0';
-
-            if (result > INT_MAX/10 || (result == INT_MAX / 10 && digit > 7))
+             int digit = str[index++]-'0';
+          
+            if (result > INT_MAX/10 || (result == INT_MAX / 10 && digit > 7)){
                     if (neg) return INT_MIN;
                     else return INT_MAX;
-
+            }
             result = result * 10 + digit;
-        }
+          }
         if(neg) {
             result=-result;
         }
         return result;
     }
+        
+        
 };
