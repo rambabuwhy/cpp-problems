cb8220800346b3afb1eda7abfc4851aff522f22b
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-02-03 00:56:33 +0530

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- LeetCode/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ LeetCode/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -9,13 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
 public:
+    
+    //1. construct node from preorder[pre-index++]
+    //2. find div_index  from inorder  (MAP)
+    //3. construct node->left  from inorder to divindex : util(inorder_start, div_index)
+    //4. construct node->right from divindex to inorder : util(div_index+1, inorder_end)
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         
         int in_start = 0;
         int in_end = inorder.size();
         
+        //value->index  of inorder
         for(int i = 0; i<inorder.size(); i++){
             M[inorder[i]] = i;
         }
@@ -33,10 +51,9 @@ public:
         return nullptr;
         
         int root_val = preorder[pre_index++];
-        TreeNode* root = new TreeNode(root_val);
-        
         int divide_index = M[root_val];
         
+        TreeNode* root = new TreeNode(root_val);
         root->left = util(preorder, inorder, in_start, divide_index);
         root->right = util(preorder, inorder, divide_index+1, in_end);
         
