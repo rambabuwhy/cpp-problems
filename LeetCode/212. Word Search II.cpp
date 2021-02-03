201279a90d649e2aec85f065ac876631daa05ee2
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-02-03 15:25:01 +0530

    Update 212. Word Search II.cpp

--- LeetCode/212. Word Search II.cpp
+++ LeetCode/212. Word Search II.cpp
@@ -4,11 +4,15 @@ Given a 2D board and a list of words from the dictionary, find all words in the 
 
 Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
 */
+
+
 class Solution {
     
     //trie data structure
     struct TrieNode {
         TrieNode *children[26];
+        
+        //store string rather than bool
         string word;
 
         TrieNode() : word("") {
@@ -19,8 +23,26 @@ class Solution {
     };
     
 public:
+        
+    //build Trie
+    TrieNode *buildTrie(vector<string> &words) {
+        TrieNode *root = new TrieNode();
+        for (int j = 0; j < words.size(); j++) {
+            string word = words[j];
+            TrieNode *curr = root;
+            for (int i = 0; i < word.length(); i++) {
+                int index = word[i] - 'a';
+                if (curr->children[index] == nullptr) {
+                    curr->children[index] = new TrieNode();
+                }
+                curr = curr->children[index];
+            }
+            curr->word = word;
+        }
+        return root;
+    }
     
-    //1. construct trie  2.  do dfs
+    //1. construct trie for words 2.  for each cell do dfs
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         TrieNode *root = buildTrie(words);
         vector<string> result;
@@ -32,17 +54,28 @@ public:
         return result;
         
     }
+
     
-    //Trie
-    TrieNode *buildTrie(vector<string> &words) {
-        TrieNode *root = new TrieNode();
-        for (int j = 0; j < words.size(); j++) {
-            string word = words[j];
-            TrieNode *curr = root;
-            for (int i = 0; i < word.length(); i++) {
-                char c = word[i] - 'a';
-                if (curr->children[c] == nullptr) {
-                    curr->children[c] = new TrieNode();
+    //dfs
+    void dfs(vector<vector<char>> &board, int i, int j, TrieNode *p, vector<string> &result) {
+        char c = board[i][j];
+        if (c == '#' || !p->children[c - 'a']) return;
+        p = p->children[c - 'a'];
+        if (p->word.size() > 0) {
+            result.push_back(p->word);
+            p->word = "";
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j, p, result);
+        if (j > 0) dfs(board, i, j - 1, p, result);
+        if (i < board.size() - 1) dfs(board, i + 1, j, p, result);
+        if (j < board[0].size() - 1) dfs(board, i, j + 1, p, result);
+        board[i][j] = c;
+    }
+private:
+    
+}; curr->children[c] = new TrieNode();
                 }
                 curr = curr->children[c];
             }
