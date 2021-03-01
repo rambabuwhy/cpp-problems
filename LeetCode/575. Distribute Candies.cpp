ec5dfc01737906fded49fff17da43d943639caaf
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-01 20:54:09 +0530

    Create 575. Distribute Candies.cpp

---
+++ LeetCode/575. Distribute Candies.cpp
@@ -0,0 +1,53 @@
+/*
+
+Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
+
+The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
+
+Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+ 
+
+Example 1:
+
+Input: candyType = [1,1,2,2,3,3]
+Output: 3
+Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
+Example 2:
+
+Input: candyType = [1,1,2,3]
+Output: 2
+Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types [1,2], [1,3], or [2,3], she still can only eat 2 different types.
+Example 3:
+
+Input: candyType = [6,6,6,6]
+Output: 1
+Explanation: Alice can only eat 4 / 2 = 2 candies. Even though she can eat 2 candies, she only has 1 type.
+
+*/
+
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        
+        unordered_map<int, int> l_map;
+        int half_size = candyType.size()/2;
+        
+        for(auto c:candyType){
+            l_map[c]++;
+        }
+        
+        int result =0;
+        
+        int map_size = l_map.size();
+        if(map_size <= half_size){
+            return map_size;
+        }
+        
+        else{
+            return half_size;
+        }
+        
+    }
+};
