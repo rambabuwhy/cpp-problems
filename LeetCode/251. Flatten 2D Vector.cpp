9357c7bf34c22d5c9bb75bf86451cd800cd69346
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-05 20:30:46 +0530

    Create 251. Flatten 2D Vector.cpp

---
+++ LeetCode/251. Flatten 2D Vector.cpp
@@ -0,0 +1,58 @@
+/*
+Design and implement an iterator to flatten a 2d vector. It should support the following operations: next and hasNext.
+
+ 
+
+Example:
+
+Vector2D iterator = new Vector2D([[1,2],[3],[4]]);
+
+iterator.next(); // return 1
+iterator.next(); // return 2
+iterator.next(); // return 3
+iterator.hasNext(); // return true
+iterator.hasNext(); // return true
+iterator.next(); // return 4
+iterator.hasNext(); // return false
+
+*/
+
+class Vector2D {
+public:
+    
+    vector<vector<int>> lv;
+    int in =0;
+    int out =0;
+    Vector2D(vector<vector<int>>& v) {
+        
+        lv= v;
+        
+    }
+    
+    int next() {
+        
+        if(!hasNext()) return -1;
+            
+        return lv[out][in++];
+        
+    }
+    
+    bool hasNext() {
+        
+        //if you move from one vector to next.  in should be 0th element and out shut be next vector
+        while(out <lv.size() && in == lv[out].size()){
+            in=0;
+            out++;
+        }
+        
+        return out < lv.size();
+        
+    }
+};
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D* obj = new Vector2D(v);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
