474bc8de960bf1bb8a42ae74c1f632ef442f8659
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-08 01:59:57 +0530

    Create 285. Inorder Successor in BST.cpp

---
+++ LeetCode/285. Inorder Successor in BST.cpp
@@ -0,0 +1,57 @@
+/*
+Given the root of a binary search tree and a node p in it, return the in-order successor of that node in the BST. If the given node has no in-order successor in the tree, return null.
+
+The successor of a node p is the node with the smallest key greater than p.val.
+
+ 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        
+        //if right exist
+        if(p->right != nullptr){
+            p = p->right;
+            
+            while(p->left) p=p->left;
+            
+            return p;
+        }
+        
+        //if no right  then use iterative inorder
+        stack<TreeNode*> lstack;
+        TreeNode* prev = nullptr;
+        
+        TreeNode* curr=root;
+        
+        while(curr || !lstack.empty()) {
+            
+            while(curr != nullptr){
+                lstack.push(curr);
+                curr=curr->left;
+            }
+            
+            curr=lstack.top();
+            lstack.pop();
+            
+            if(prev == p) return curr;
+            
+            prev=curr;
+            
+            curr=curr->right;
+        }
+        
+        return nullptr;
+        
+    }
+};
