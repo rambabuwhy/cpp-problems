dea849af1a6212d051acc470c98534932f00ba3b
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-13 11:27:27 +0530

    Create Three Number Sum.cpp

---
+++ algoexpert/Three Number Sum.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+
+void util(vector<int> &array, vector<vector<int>> &result, int targetSum, int index ){
+		
+	int left = index+1;
+	int right = array.size()-1;
+	while ( left < right) {
+			int currSum = array[left] + array[right] + array[index];
+		
+			if( targetSum == currSum) {
+				vector<int> currSet({array[left], array[right], array[index] });
+				sort(currSet.begin(), currSet.end());
+				result.push_back(currSet);
+				left++;
+				right--;
+				
+			}
+			else if(currSum > targetSum){
+				right--;
+			}
+			else {
+				left++;
+			}
+			
+		}
+		
+}
+vector<vector<int>> threeNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+	if(array.size() < 3) return vector<vector<int>>();
+	sort(array.begin(), array.end());
+	
+	vector<vector<int>> result;
+	for(int i =0; i<array.size(); i++){
+		util(array, result, targetSum, i);
+	}
+	return result;
+}
+
+	
+	
