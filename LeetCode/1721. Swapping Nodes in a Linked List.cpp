f94e42a80ec151a72abe7c7774b342eb7d96b367
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-14 21:35:41 +0530

    Create 1721. Swapping Nodes in a Linked List.cpp

---
+++ LeetCode/1721. Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/*
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+Example 3:
+
+Input: head = [1], k = 1
+Output: [1]
+Example 4:
+
+Input: head = [1,2], k = 1
+Output: [2,1]
+Example 5:
+
+Input: head = [1,2,3], k = 2
+Output: [1,2,3]
+
+*/
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        
+        if(head == nullptr || head->next == nullptr){
+          return head;  
+        } 
+        
+        ListNode* curr = head;
+        
+        //find length
+        int listLen = 0;
+        while(curr){
+            listLen++;
+            curr = curr->next;
+        }
+        
+        //find first node
+        ListNode* firstNode=head;
+        for(int i=1; i<k; i++){
+            firstNode = firstNode->next;
+        }
+        
+        //find lastNode
+        ListNode* lastNode=head;
+        for(int i=0; i<listLen-k; i++){
+            lastNode = lastNode->next;
+        }
+        
+        swap(firstNode->val, lastNode->val);
+        
+        return head;
+        
+    }
+};
