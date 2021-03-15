34ef570a8b6544bb598ffecbe6270fac36dd73a6
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-15 18:17:18 +0530

    Create 535. Encode and Decode TinyURL.cpp

---
+++ LeetCode/535. Encode and Decode TinyURL.cpp
@@ -0,0 +1,34 @@
+/*
+
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+
+*/
+
+class Solution {
+public:
+    
+    hash<string> lhash;
+    unordered_map<string, string> lmap; 
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        
+        string key = to_string( lhash(longUrl) );
+        lmap[ key ] = longUrl;
+        return key;
+        
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        return lmap[ shortUrl ];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
