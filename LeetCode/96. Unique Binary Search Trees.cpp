d710162dac5c7c9a4f6e785e3d1f408f62c078b8
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-22 00:37:27 +0530

    Update 96. Unique Binary Search Trees.cpp

--- LeetCode/96. Unique Binary Search Trees.cpp
+++ LeetCode/96. Unique Binary Search Trees.cpp
@@ -13,7 +13,6 @@ public:
         dp[0] = dp[1] = 1;
         
         for (int i = 2; i <= n; i++) {
-            dp[i] = 0;
             for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j-1] * dp[i-j];
             }
