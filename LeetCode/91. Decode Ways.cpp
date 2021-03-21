c52a7c48bc28df5d9267b528b416815ced1e86b9
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-21 12:51:55 +0530

    Create 91. Decode Ways.cpp

---
+++ LeetCode/91. Decode Ways.cpp
@@ -0,0 +1,81 @@
+/*
+
+A message containing letters from A-Z can be encoded into numbers using the following mapping:
+
+'A' -> "1"
+'B' -> "2"
+...
+'Z' -> "26"
+To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+
+"AAJF" with the grouping (1 1 10 6)
+"KJF" with the grouping (11 10 6)
+Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+
+Given a string s containing only digits, return the number of ways to decode it.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: "12" could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+Example 3:
+
+Input: s = "0"
+Output: 0
+Explanation: There is no character that is mapped to a number starting with 0.
+The only valid mappings with 0 are 'J' -> "10" and 'T' -> "20", neither of which start with 0.
+Hence, there are no valid ways to decode this since all digits need to be mapped.
+Example 4:
+
+Input: s = "06"
+Output: 0
+Explanation: "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06").
+
+*/
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        
+        if(s.length()==0) {
+            return 0;
+        }
+        if(s.length()==1) {
+            return s[0]=='0'?0:1;
+        }
+        
+        vector<int>dp(s.length()+1,0);
+        dp[0] = 1;
+        dp[1] = (s[0]=='0')?0:1;
+        
+        for(int i = 2; i<s.length()+1; i++)
+        {
+            //one digit
+            int oneDigit = stoi(s.substr(i-1,1));
+            if(oneDigit != 0) {
+                dp[i] += dp[i-1];
+            }
+            
+            //two digit
+            int twoDigit = stoi(s.substr(i-2,2));
+            if( twoDigit >=10 && twoDigit <=26) {
+                if(s[i-2]!='0'){
+                    dp[i] += dp[i-2];
+                }
+            }
+        }
+        
+        return dp[s.length()];
+        
+    }
+};
