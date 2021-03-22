8c875198da24967fa40f9f6794404ce05ae45e4f
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-22 13:07:25 +0530

    Create 896. Monotonic Array.cpp

---
+++ LeetCode/896. Monotonic Array.cpp
@@ -0,0 +1,49 @@
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+
+Return true if and only if the given array A is monotonic.
+
+ 
+
+Example 1:
+
+Input: [1,2,2,3]
+Output: true
+Example 2:
+
+Input: [6,5,4,4]
+Output: true
+Example 3:
+
+Input: [1,3,2]
+Output: false
+Example 4:
+
+Input: [1,2,4,5]
+Output: true
+Example 5:
+
+Input: [1,1,1]
+Output: true
+
+*/
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        
+        bool increase = true;
+        bool decrease = true;
+            
+        for(int i=0; i<A.size()-1; i++){
+            
+            if(A[i] > A[i+1]) increase = false;
+            if(A[i] < A[i+1]) decrease = false;
+        }
+        
+        return increase || decrease;
+        
+    }
+};
