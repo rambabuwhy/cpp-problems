45a756da2559a3799216f3d0fad9c60753f1529a
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-03-28 16:42:35 +0530

    Update 54. Spiral Matrix.cpp

--- LeetCode/54. Spiral Matrix.cpp
+++ LeetCode/54. Spiral Matrix.cpp
@@ -29,16 +29,22 @@ public:
         
         while (r1 <= r2 && c1 <= c2) {
             
+            //left to right
             for (int c = c1; c <= c2; c++) 
             ans.push_back(matrix[r1][c]);
             
+            //top to bottom
             for (int r = r1 + 1; r <= r2; r++) 
             ans.push_back(matrix[r][c2]);
             
+            
             if (r1 < r2 && c1 < c2) {
+                
+                //right to left
                 for (int c = c2 - 1; c >= c1; c--) 
                 ans.push_back(matrix[r2][c]);
                 
+                //bottom to top
                 for (int r = r2-1; r > r1; r--)
                 ans.push_back(matrix[r][c1]);
             }
