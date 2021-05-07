825d6228d8417b98ae0daa9bc85701125a771d78
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-05-07 23:09:51 +0530

    Update 169. Majority Element.cpp

--- LeetCode/169. Majority Element.cpp
+++ LeetCode/169. Majority Element.cpp
@@ -1,16 +1,34 @@
+/*Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+*/
+
+
 class Solution {
 public:
+    //use map: element -> frequesnce
     int majorityElement(vector<int>& nums) {
         unordered_map<int,int> M;
         for(auto i:nums){
             M[i]++;            
         }
-        for(auto i:M){
+        for(auto itr:M){
             if(i.second > nums.size()/2){
-                return i.first;
+                return itr.first;
             }
         }
-        return -1;;
-        
+        return -1;
     }
 };
