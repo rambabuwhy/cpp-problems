b18ac7562eacfd02b66cb86de05ab3165cebaf5b
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-06-11 12:47:14 +0530

    Create 45. Jump Game II.cpp

---
+++ LeetCode/45. Jump Game II.cpp
@@ -0,0 +1,42 @@
+/*
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+You can assume that you can always reach the last index.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+*/
+
+class Solution {
+public:
+    //case: sure we can reach end, but find minimum steps
+    //Update farthest to i + nums[i] if the latter is larger.
+    //If we reach currentJumpEnd, it means we finished the current jump, and can begin checking the next jump by setting currentJumpEnd = farthest
+    int jump(vector<int>& nums) {
+        int resultCount =0;
+        int farthest = 0;
+        int currJumpEnd =0;
+        
+        for(int i=0; i<nums.size()-1; i++){  
+            farthest = max(farthest, i+nums[i]);
+            if(i==currJumpEnd){
+                resultCount++;
+                currJumpEnd = farthest;
+            }
+        }
+        return resultCount; 
+    }
+};
