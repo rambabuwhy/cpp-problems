1c05a4d788d3ad1e5f9819970d335e8fe83c8127
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-06-30 10:48:31 +0530

    Update 1. Two Sum.cpp

--- LeetCode/1. Two Sum.cpp
+++ LeetCode/1. Two Sum.cpp
@@ -16,19 +16,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        if(nums.size()<2) return {};
+        if(nums.size() < 2) return vector<int>();
         
+        //use map: <num, index>
         unordered_map<int,int> map;
         
         for(int i=0;i<nums.size();i++){
+            
             auto itr = map.find(target-nums[i]);
             if(itr != map.end()){
                 return {i,itr->second};                
             }
             else{
-                map[nums[i]]=i;
+                map[nums[i]] = i;
             }
         }
-        return {};
+        return vector<int>();
     }
 };
