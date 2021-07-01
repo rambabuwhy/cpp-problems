68cec9f547c5be8c3359443dd9357b966a95b621
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-02 00:08:48 +0530

    Update 204. Count Primes.cpp

--- LeetCode/204. Count Primes.cpp
+++ LeetCode/204. Count Primes.cpp
@@ -2,34 +2,44 @@
 
 Count the number of prime numbers less than a non-negative number, n.
 
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
 */
 
 class Solution {
 public:
     int countPrimes(int n) {
         
-        if(n<2)  return 0;
+        // Sieve of Eratosthenes
+        vector<bool> mat(n, true);
         
-        vector<bool> dp(n,true);
-        
-        dp[0]  = false;
-        dp[1] =  false;
-        
-        for(int i = 2; i<sqrt(n); i++){
+        for(int i=2; i*i <n; i++) {
             
-            if(dp[i] == true){
-                
-                for(int j=i*i;  j<n;  j=j+i){
-                    dp[j]= false;
+            //all are true at first
+            if(mat[i] == true) {
+                for(int j=i*i; j<n; j=j+i) {
+                    mat[j]=false;
                 }
-               
             }
         }
         
-        int result = count(dp.begin(),dp.end(),true);
-        cout<<result;
-        
-        return  result;
-        
+        int count =0;
+        for(int i=2; i<n; i++) { //dont forget it start with 2
+            if(mat[i] == true) count++;
+        }  
+        return count;
     }
 };
