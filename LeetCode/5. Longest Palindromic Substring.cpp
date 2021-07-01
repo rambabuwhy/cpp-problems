d2fa8bf6c56b0083b7dd040fab032d26e9e14474
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-02 01:50:55 +0530

    Update 5. Longest Palindromic Substring.cpp

--- LeetCode/5. Longest Palindromic Substring.cpp
+++ LeetCode/5. Longest Palindromic Substring.cpp
@@ -17,48 +17,46 @@ Output: "bb"
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.length()<=1){
+        
+        if(s.length() <= 1){
             return s;
         }
         
         vector<vector<bool>> dp(s.length(),vector<bool>(s.length()));
         string result;
-        int maxLength=0;
+        int maxLength = 0;
         
-        //store single char in result
-        //no need to store all  single char: first single char as result of length 1
+        // every string with one character is a palindrome
         for(int i=0; i<s.length(); i++){
             
-            dp[i][i]=true;
+            dp[i][i] = true;
             
+            //save that string  and  length
             if(maxLength < 1){
-                result=s[i];
-                maxLength=1;
-                               
-            }
-            
+                result = s[i];
+                maxLength = 1;                   
+            } 
         }
 
-        for(int startIndex = s.length()-1; startIndex>=0; startIndex--){
-            for(int endIndex = startIndex+1;  endIndex<s.length();  endIndex++){
-                //palindrome : compare last two char
-                if(s[startIndex]==s[endIndex]){ 
+        //logic
+        for(int startIndex = s.length()-1; startIndex >= 0; startIndex--){
+            for(int endIndex = startIndex+1;  endIndex < s.length();  endIndex++){
+                
+                if(s[startIndex]==s[endIndex]) { //palindrome : compare last two char
                     //then remaining string should be palindrome
                     if( dp[startIndex+1][endIndex-1] || (endIndex-startIndex)==1) {  
-                        dp[startIndex][endIndex]=true;
-                        if(maxLength < endIndex-startIndex+1){
+                        
+                        dp[startIndex][endIndex] = true;
+                        if(maxLength < endIndex-startIndex+1) {
+                            
+                            //save string and length
                             result=s.substr(startIndex, endIndex-startIndex+1);
                             maxLength=max(maxLength, endIndex-startIndex+1);
-
                         }
-                    
-                    }
-                    
+                    } 
                 }
-               
             }
         }
         return result;
-        
     }
 };
