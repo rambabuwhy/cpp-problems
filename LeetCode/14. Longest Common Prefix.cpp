8df5d4e715983e20220d01d19f49893da9ad50ce
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-01 12:54:20 +0530

    Update 14. Longest Common Prefix.cpp

--- LeetCode/14. Longest Common Prefix.cpp
+++ LeetCode/14. Longest Common Prefix.cpp
@@ -21,33 +21,33 @@ Explanation: There is no common prefix among the input strings.
 class Solution {
 public:
     
-    //Divide and conquer
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size() == 0 ) return "";
+        if(strs.size() == 0) return "";
         if(strs.size() == 1) return strs[0];
-        return longestCommonPrefixUtil(strs,0,strs.size()-1);
-        
+        return longestCommonPrefixUtil(strs, 0, strs.size()-1);
     }
-    string longestCommonPrefixUtil(vector<string> & strs,int l, int r){
-        if(l==r){
+    
+    //divide and conquer
+    string longestCommonPrefixUtil(vector<string>& strs,int l, int r) {
+        
+        if(l == r){
             return strs[l];
-        }
-        else{
+        } else {
             int mid = (l + r) / 2;
-            string str1 = longestCommonPrefixUtil(strs,l,mid);
-            string str2 = longestCommonPrefixUtil(strs,mid+1,r);
-            return commonPrefix(str1,str2);
+            string str1 = longestCommonPrefixUtil(strs, l, mid);
+            string str2 = longestCommonPrefixUtil(strs, mid+1, r);
+            return commonPrefix(str1, str2);
         }
-        
     }
-     string commonPrefix(string s1, string s2){
-        int minimum = min(s1.length(),s2.length());
+    
+    //iterate with minimum length of two strings
+    string commonPrefix(string s1, string s2) {
+        int minimum = min(s1.length(), s2.length());
         string result;
-        for (int i=0;i<minimum;i++){
+        for (int i=0; i<minimum; i++){
             if(s1[i] != s2[i]) break;
             else result += s1[i];
         }
         return result;
     }
-   
 };
