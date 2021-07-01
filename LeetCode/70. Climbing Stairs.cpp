03204271dbe253467ea13b28e71bc887bf2f9222
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-01 22:27:07 +0530

    Update 70. Climbing Stairs.cpp

--- LeetCode/70. Climbing Stairs.cpp
+++ LeetCode/70. Climbing Stairs.cpp
@@ -28,15 +28,14 @@ public:
         
         if(n==0) return 1;
         if(n==1) return 1;
-        //if(n==2) return 2;
+        
         vector<int> dp(n+1);
         dp[0]=1;
         dp[1]=1;
+        
         for(int i = 2; i <= n; i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
-        
         return dp[n];
-        
     }
 };
