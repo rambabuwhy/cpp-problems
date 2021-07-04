d07dc68f53b0c7cbdf5ee92dc0f7f57d71a6f552
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-04 22:49:38 +0530

    Update 78. Subsets.cpp

--- LeetCode/78. Subsets.cpp
+++ LeetCode/78. Subsets.cpp
@@ -1,7 +1,18 @@
 /*
-Given a set of distinct integers, nums, return all possible subsets (the power set).
+Given an integer array nums of unique elements, return all possible subsets (the power set).
 
-Note: The solution set must not contain duplicate subsets.
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
 
 */
 
@@ -11,16 +22,20 @@ public:
         
         vector<vector<int>> result;
         vector<int> curr;
-        subsetsUtil(nums,result,curr,0);
+        subsetsUtil(nums, result, curr, 0);
         
-        return result;
-        
+        return result;  
     }
-    void subsetsUtil(vector<int> & nums,vector<vector<int>>&result, vector<int>&curr, int start){
+    
+    void subsetsUtil(vector<int> & nums, vector<vector<int>>&result, vector<int>&curr, int start){
+        
         result.push_back(curr);
+        
+        //why i+1?  i has limit i<nums.size in for loop
+        //if start+1;  no limit; recusrsion goin in calling for all infinite values
         for(int i = start; i<nums.size(); i++){
             curr.push_back(nums[i]);
-            subsetsUtil(nums,result,curr,i+1);
+            subsetsUtil(nums, result, curr, i+1);
             curr.pop_back();
         }
     }
