ecad912d0577df620235d4d1b8d93c2641ed3204
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-05 10:41:49 +0530

    Update 79. Word Search.cpp

--- LeetCode/79. Word Search.cpp
+++ LeetCode/79. Word Search.cpp
@@ -1,7 +1,26 @@
 /*
-Given a 2D board and a word, find if the word exists in the grid.
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
 */
 
 class Solution {
