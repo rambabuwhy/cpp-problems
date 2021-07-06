02b92909f4ff7fe1a037804ec8dacd8f6fe3b317
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-07 01:53:46 +0530

    Update 240. Search a 2D Matrix II.cpp

--- LeetCode/240. Search a 2D Matrix II.cpp
+++ LeetCode/240. Search a 2D Matrix II.cpp
@@ -10,25 +10,16 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        
+        //start from bottom left; go up and right
         int r = matrix.size()-1;
         int c = 0;
         
         while(c<matrix[0].size() && r >=0){
             
-            if(matrix[r][c] > target){
-                r--;
-            }
-            else if(matrix[r][c] < target){
-                c++;
-            }
-            else{
-                return true;
-            }
-            
-        }
-        
-        return false;
-        
+            if(matrix[r][c] > target)  r--;
+            else if(matrix[r][c] < target) c++;
+            else return true;
+        }  
+        return false; 
     }
 };
