06f9899b65ccd6484097fa91781365472c497d41
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-11 13:21:19 +0530

    Update 41. First Missing Positive.cpp

--- LeetCode/41. First Missing Positive.cpp
+++ LeetCode/41. First Missing Positive.cpp
@@ -19,6 +19,29 @@ Input: nums = [7,8,9,11,12]
 Output: 1
 
 */
+
+// *********SWAP METHOD************
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        
+        // swap numbers into its right place
+        int n = nums.size();
+        for(int i = 0; i < n; ++ i){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums[i], nums[nums[i] - 1]);
+            }    
+        }
+            
+        //return if number is not in its right place
+        for(int i = 0; i < n; ++ i){
+            if(nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+};
+
+//*********SIGN METHOD*********
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
