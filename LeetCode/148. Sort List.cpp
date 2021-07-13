50bb4d4c56f7e74bc67c9fd59cd3329e8f99fdc9
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-14 00:14:33 +0530

    Create 148. Sort List.cpp

---
+++ LeetCode/148. Sort List.cpp
@@ -0,0 +1,81 @@
+/*
+
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+
+
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+        if (!head || !head->next) return head;
+        
+        ListNode* mid = getMid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *curr = dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+        
+        if(list1) curr->next = list1;
+        else curr->next = list2;
+
+        return dummy->next;
+    }
+
+    ListNode* getMid(ListNode* head) {
+        ListNode* midPrev = nullptr;
+        while (head && head->next) {
+            midPrev = (midPrev == nullptr) ? head : midPrev->next;
+            head = head->next->next;
+        }
+        ListNode* mid = midPrev->next;
+        midPrev->next = nullptr;
+        return mid;
+    }
+};
