0d5d6d7a0cb127110d950f87ec03a5efce00fdaf
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-16 12:48:36 +0530

    Update 23. Merge k Sorted Lists.cpp

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -40,6 +40,17 @@ Output: []
  * };
  */
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 struct compare{
     
     bool operator()(ListNode* l, ListNode* r){
@@ -51,43 +62,31 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        priority_queue< ListNode*,
-                        vector<ListNode*>,
-                        compare> pq;
-        
-        
-        for(int i=0; i<lists.size(); i++){
-            
-            if(lists[i])
-            pq.push(lists[i]);
+        //priority queue:  push all list pointers means push pointer to first node
+        //comparator: top element is minimum node value
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for(int i=0; i<lists.size(); i++){ 
+            if(lists[i]) pq.push(lists[i]);
         }
         
-        if(pq.empty())
-        return nullptr;
+        if(pq.empty()) return nullptr;
         
+        //result is always outside of the priority queue: 
+        //add from top of the priority queue
         ListNode* result = pq.top();
         pq.pop();
-              
-        if(result->next)
-        pq.push(result->next);
         
+        //save head; because result will move forwors as node added
         ListNode* new_head = result;
-        
+        if(result->next) pq.push(result->next);
         while(!pq.empty()){
             
-            result->next=pq.top();
+            result->next = pq.top();
             pq.pop();
-            
             result = result->next;
             
-            if(result->next)
-            pq.push(result->next);
-            
-            
+            if(result->next) pq.push(result->next);   
         }
-        
         return new_head;
-        
-        
     }
 };
