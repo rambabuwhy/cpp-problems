b6bf45fd8cfe5934fd797163072c144893af6ff7
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-07-17 20:32:18 +0530

    Update 4. Median of Two Sorted Arrays.cpp

--- LeetCode/4. Median of Two Sorted Arrays.cpp
+++ LeetCode/4. Median of Two Sorted Arrays.cpp
@@ -23,39 +23,43 @@ The median is (2 + 3)/2 = 2.5
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& input1, vector<int>& input2) {
-
-        if (input1.size() > input2.size()) {
-            return findMedianSortedArrays(input2, input1);
-        }
+            
+        //make always input 2  big
+        if (input1.size() > input2.size()) return findMedianSortedArrays(input2, input1);
+        
         int x = input1.size();
         int y = input2.size();
 
         int low = 0;
         int high = x;
         while (low <= high) {
-            int partitionX = (low + high)/2;
-            int partitionY = (x + y + 1)/2 - partitionX;
-
-            int maxLeftX = (partitionX == 0) ? INT_MIN : input1[partitionX - 1];
-            int minRightX = (partitionX == x) ? INT_MAX : input1[partitionX];
-
-            int maxLeftY = (partitionY == 0) ? INT_MIN : input2[partitionY - 1];
-            int minRightY = (partitionY == y) ? INT_MAX : input2[partitionY];
-
+            
+            //Partition X  
+            int x_partition = (low + high)/2;
+            
+            //Partition Y 
+            int y_partition = (x + y + 1)/2 - x_partition;
+            
+            //variables which partition x 
+            int maxLeftX = (x_partition == 0) ? INT_MIN : input1[x_partition - 1];
+            int minRightX = (x_partition == x) ? INT_MAX : input1[x_partition];
+            
+            //variables which partition y
+            int maxLeftY = (y_partition == 0) ? INT_MIN : input2[y_partition  - 1];
+            int minRightY = (y_partition == y) ? INT_MAX : input2[y_partition ];
+            
             if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-
-                if ((x + y) % 2 == 0) {
-                    return ((double)max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2;
-                } else {
-                    return (double)max(maxLeftX, maxLeftY);
-                }
-            } else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+                // right way of partition
+                if ((x + y) % 2 == 0) return ((double)max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2;
+                else return (double)max(maxLeftX, maxLeftY);
+            } else if (maxLeftX > minRightY) { 
+                //move leftside
+                high = x_partition - 1;
+            } else { 
+                //move rightside
+                low = x_partition + 1;
             }
         }
         return -1;
-
     }
 };
