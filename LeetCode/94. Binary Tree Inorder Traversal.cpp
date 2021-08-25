ffe3518c00d9f39ab3b8c7f4755f2ce127775553
Rambabu Yerajana <rambabuwhy@gmail.com>
2021-08-25 23:39:46 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- LeetCode/94. Binary Tree Inorder Traversal.cpp
+++ LeetCode/94. Binary Tree Inorder Traversal.cpp
@@ -14,29 +14,28 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         
-        if(root == nullptr)
-        return vector<int>();
+        if(root == nullptr) return vector<int>();
         
         vector<int> result;
-        stack<TreeNode*> S;
+        stack<TreeNode*> stack;
         
         TreeNode*curr = root;
-        while(curr || !S.empty()){
+        while(curr || !stack.empty()){
             
             while(curr){
-                S.push(curr);
+                stack.push(curr);
                 curr = curr->left;
             }
             
-            curr = S.top();
-            S.pop();
+            curr = stack.top();
+            stack.pop();
             
             result.push_back(curr->val);
-            
             curr= curr->right;   
         }
         return result;
