227788c397fe049f821957d301f87f7152967d77
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-02-09 15:30:27 +0530

    Create 541. Reverse String II.cpp

---
+++ LeetCode/541. Reverse String II.cpp
@@ -0,0 +1,29 @@
+/*
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+ 
+
+Example 1:
+
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Example 2:
+
+Input: s = "abcd", k = 2
+Output: "bacd"
+
+*/
+
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        
+        for (int i = 0; i < s.size(); i += 2*k) {
+            reverse(s.begin()+i, min(s.begin()+i+k, s.end()));
+        }
+        return s;
+    }
+};
