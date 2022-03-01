efa056130dea875dcce1c7c9039bafec7610f2c6
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-01 11:41:18 +0530

    Update 338. Counting Bits.cpp

--- LeetCode/338. Counting Bits.cpp
+++ LeetCode/338. Counting Bits.cpp
@@ -19,12 +19,10 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         
-        vector<int> result(num+1,0);
-        for(int i = 1; i <= num; i++){
-            result[i]=result[(i & (i-1))] +1;
-        }
-        
+        vector<int> result(num+1, 0);
+        for(int i=1; i<=num; i++){
+            result[i] = result[(i & (i-1))] + 1;
+        }  
         return result;
-        
     }
 };
