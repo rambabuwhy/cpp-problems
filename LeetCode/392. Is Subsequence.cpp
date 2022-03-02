651fa186987070cbf9210bd6d90909656740f3b6
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-02 11:28:38 +0530

    Update 392. Is Subsequence.cpp

--- LeetCode/392. Is Subsequence.cpp
+++ LeetCode/392. Is Subsequence.cpp
@@ -7,17 +7,9 @@ Follow up:
 If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
 
 */
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-     
-        if(s.empty() && !t.empty()) return true;
-        if(s.empty() && t.empty()) return true;
-        if(!s.empty() && t.empty()) return false;
-        //if(!s.empty() && !t.empty());
-        
-        cout<<s.size();
-           
+
+bool isSubsequence(string s, string t) {
+
         int ll = 0 , lr = s.size();
         int rl = 0,  rr = t.size();
         while(ll < lr && rl < rr){
@@ -25,5 +17,4 @@ public:
             rl++;
         }
         return ll == lr;
-    }
-};
+}
