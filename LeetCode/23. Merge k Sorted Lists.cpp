c32f05b6688df2f94d5bde09d36e67c6d01e17ad
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-09 13:25:40 +0530

    Update 23. Merge k Sorted Lists.cpp

--- LeetCode/23. Merge k Sorted Lists.cpp
+++ LeetCode/23. Merge k Sorted Lists.cpp
@@ -40,31 +40,20 @@ Output: []
  * };
  */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
-struct compare{
-    
-    bool operator()(ListNode* l, ListNode* r){
-        return l->val > r->val;
+//compare function for priority queue
+struct compare {
+    bool operator() (ListNode* a, ListNode* b){
+        return (a->val) > (b->val);
     }
-    
 };
+
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
         //priority queue:  push all list pointers means push pointer to first node
         //comparator: top element is minimum node value
-        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq; //min q
         for(int i=0; i<lists.size(); i++){ 
             if(lists[i]) pq.push(lists[i]);
         }
@@ -72,21 +61,24 @@ public:
         if(pq.empty()) return nullptr;
         
         //result is always outside of the priority queue: 
-        //add from top of the priority queue
         ListNode* result = pq.top();
         pq.pop();
         
-        //save head; because result will move forwors as node added
-        ListNode* new_head = result;
+        ListNode* head = result;
+        
+        //After pop from Q , push next node in Q from same list.
         if(result->next) pq.push(result->next);
         while(!pq.empty()){
             
             result->next = pq.top();
             pq.pop();
-            result = result->next;
             
-            if(result->next) pq.push(result->next);   
+            //After pop from Q , push next node in Q from same list.
+            result = result->next;            
+            if(result->next) {
+                pq.push(result->next);   
+            }
         }
-        return new_head;
+        return head;
     }
 };
