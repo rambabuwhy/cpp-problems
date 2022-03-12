bec9fa2852e2ae2b92d7617bb9482c082836057e
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-12 12:02:19 +0530

    Update 138. Copy List with Random Pointer.cpp

--- LeetCode/138. Copy List with Random Pointer.cpp
+++ LeetCode/138. Copy List with Random Pointer.cpp
@@ -30,49 +30,45 @@ public:
 class Solution {
 public:
     
+    // Visited dictionary to hold old node reference as "key" and new node reference as the "value"
     unordered_map<Node*, Node*> umap;
     
     Node* getClonedNode(Node* node){
         
+        // If the node exists then
         if(node){
-            
+            // Check if the node is in the visited dictionary
             if(umap.find(node) !=  umap.end()){
+                // If its in the visited dictionary then return the new node reference from the dictionary
                 return umap[node];  
             }
-            else{ 
+            else{
+                // Otherwise create a new node, add to the dictionary and return it
                 umap[node] = new Node(node->val);
                 return umap[node];
             } 
-        }
-        
-        return nullptr;
-        
-        
+        }        
+        return nullptr;    
     }
     
     Node* copyRandomList(Node* head) {
         
-        if(head == nullptr)
-        return head;
+        if(head == nullptr) return head;
         
         Node* oldNode = head;
-        Node* newNode = new Node(oldNode->val);
-        
+        Node* newNode = new Node(oldNode->val);        
         umap[oldNode] = newNode;
         
+        // Iterate on the linked list until all nodes are cloned.
         while(oldNode){
-            
+            // Get the clones of the nodes referenced by random and next pointers.
             newNode->random = getClonedNode(oldNode->random);
             newNode->next = getClonedNode(oldNode->next);
             
+            // Move one step ahead in the linked list.
             oldNode = oldNode->next;
-            newNode = newNode->next;
-            
-            
-            
-        }
-        
-        return umap[head];
-        
+            newNode = newNode->next;       
+        }        
+        return umap[head];        
     }
 };
