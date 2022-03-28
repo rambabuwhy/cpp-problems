8845d59389f15b3ec50dd5e79421707f88afcb7f
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-28 11:23:01 +0530

    Update 81. Search in Rotated Sorted Array II.cpp

--- LeetCode/81. Search in Rotated Sorted Array II.cpp
+++ LeetCode/81. Search in Rotated Sorted Array II.cpp
@@ -22,42 +22,23 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
        
-        int left = 0, right =  nums.size()-1, mid;
-        
-        while( left <= right ){
+        int left = 0, right =  nums.size()-1, mid;        
+        while( left <= right ) {
             
-            mid = left + ( right - left) / 2; 
+            mid = left + ( right - left) / 2;             
+            if(nums[mid] == target) return true;
             
-            if(nums[mid] == target){
-                return true;
-            } 
-
-            
-            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {
+            if((nums[left] == nums[mid]) && (nums[right] == nums[mid])) {
                 left++; 
                 right--;
-            }
-
-            else if(nums[left] <= nums[mid])
-            {
-                if( (nums[left]<=target) && (nums[mid] > target) ){
-                    right = mid-1;
-                } 
-                else {
-                    left = mid + 1; 
-                }
-            }
-            else
-            {
-                if((nums[mid] < target) &&  (nums[right] >= target) ) {
-                    left = mid+1;
-                }
-                else{
-                    right = mid-1;
-                } 
+            }else if(nums[left] <= nums[mid]) {
+                if( (nums[left]<=target) && (nums[mid] > target) )  right = mid-1;
+                else left = mid + 1; 
+            }else {
+                if((nums[mid] < target) &&  (nums[right] >= target) )  left = mid+1;
+                else right = mid-1;
             }
         }
-        return false;
-        
+        return false;        
     }
 };
