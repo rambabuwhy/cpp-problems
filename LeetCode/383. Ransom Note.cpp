734b21e7dc5b4afe6c9e75bbfa7b664566a22ab1
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-29 16:45:07 +0530

    Update 383. Ransom Note.cpp

--- LeetCode/383. Ransom Note.cpp
+++ LeetCode/383. Ransom Note.cpp
@@ -1,23 +1,44 @@
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+
 class Solution {
 public:
+    //insert magazine into set; traverse ransom note and verify if it exist in set
     bool canConstruct(string ransomNote, string magazine) {
         
         if(ransomNote.empty() && magazine.empty()) return true;
         if(magazine.empty()) return false;
         
-        unordered_multiset<char> mag;
+        unordered_multiset<char> lset;
         for(auto c:magazine){
-            mag.emplace(c);
-            
+            lset.emplace(c);            
         }
+        
         for(auto c:ransomNote){
-            unordered_multiset<char> ::iterator itr = mag.find(c);
-            if(itr == mag.end()){
+            unordered_multiset<char> ::iterator itr = lset.find(c);
+            if(itr == lset.end()){
                 return false;
             }
-            mag.erase(itr);
+            lset.erase(itr);
         }
-        return true;
-        
+        return true;        
     }
 };
