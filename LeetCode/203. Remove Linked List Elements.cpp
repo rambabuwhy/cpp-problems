8afa59d0b289eb51cf58c056db8e6e33fb484488
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-03-29 18:33:53 +0530

    Update 203. Remove Linked List Elements.cpp

--- LeetCode/203. Remove Linked List Elements.cpp
+++ LeetCode/203. Remove Linked List Elements.cpp
@@ -1,5 +1,21 @@
 /*
-Remove all elements from a linked list of integers that have value val.
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
 
 */
 
@@ -17,18 +33,17 @@ Remove all elements from a linked list of integers that have value val.
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
+        
         if(head == nullptr) return nullptr;
         
         //dummy ListHead    
         ListNode* dummy = new ListNode(0);
-        dummy->next = head;
+        dummy->next = head;        
         
-        
-        //three pointers  1. traverse  2.previous 3.delete
+        //three pointers  1. previous  2. current 3.delete
         ListNode *prev = dummy;
         ListNode *curr = head;
-        ListNode *del = nullptr;
-        
+        ListNode *del = nullptr;        
         
         while(curr != nullptr){
             
@@ -44,15 +59,12 @@ public:
             //move curr pointer
             curr = curr->next;
             
-            //delete del pointer
+            //garbage: delete del pointer
             if(del != nullptr){
                 delete del;
                 del = nullptr;
-            }
-     
-        }
-        
+            }     
+        }        
         return dummy->next;
-
     }
 };
