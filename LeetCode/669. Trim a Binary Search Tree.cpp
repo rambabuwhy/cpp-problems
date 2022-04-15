b80e094dbfb0e0cc4c1441d30b01a37ac27d87bd
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-15 13:24:08 +0530

    Update 669. Trim a Binary Search Tree.cpp

--- LeetCode/669. Trim a Binary Search Tree.cpp
+++ LeetCode/669. Trim a Binary Search Tree.cpp
@@ -47,10 +47,13 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         
-        if(root == nullptr) return nullptr;        
+        if(root == nullptr) return nullptr; 
+        
+        //trim
         if(root->val > high) return trimBST(root->left, low, high);        
         if(root->val < low)  return trimBST(root->right, low, high);
         
+        //contruct after trim or as it is
         root->left = trimBST(root->left, low, high);
         root->right = trimBST(root->right, low, high);
         
