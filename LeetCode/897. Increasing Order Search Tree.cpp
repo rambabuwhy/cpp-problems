5d866d070ae60700e95dddcb1c06c95a703d4f62
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-17 10:59:53 +0530

    Update 897. Increasing Order Search Tree.cpp

--- LeetCode/897. Increasing Order Search Tree.cpp
+++ LeetCode/897. Increasing Order Search Tree.cpp
@@ -1,7 +1,19 @@
 /*
 
-iven the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
 
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
 */
 
 
@@ -16,29 +28,28 @@ iven the root of a binary search tree, rearrange the tree in in-order so that th
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
 class Solution {
 public:
     TreeNode* increasingBST(TreeNode* root) {
         
-        if(root == nullptr)  return nullptr;
-        
+        if(root == nullptr)  return nullptr;        
         increasingBST(root->left);
         
-        if(newRoot == nullptr)
-        newRoot = new TreeNode(0), curr = newRoot;
-        
+        if(newRoot == nullptr){
+            newRoot = new TreeNode(0);
+            curr = newRoot;
+        }                
         
         root->left = nullptr;
         curr->right = root;
-        curr = root;
-        
-        
+        curr = root;        
             
-        increasingBST(root->right);
-        
-        return newRoot->right;
-        
+        increasingBST(root->right);        
+        return newRoot->right;        
     }
+    
 private:
     TreeNode *newRoot;
     TreeNode *curr;
