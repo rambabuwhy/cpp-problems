d800d6b41ace4a8e7c4d5dac3818f9776162f467
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-20 18:36:07 +0530

    Update 278. First Bad Version.cpp

--- LeetCode/278. First Bad Version.cpp
+++ LeetCode/278. First Bad Version.cpp
@@ -31,16 +31,13 @@ public:
     
     //use binary search
     int firstBadVersion(int n) {
+        
         int left = 1;
         int right = n;
-        while( left < right){
-            
+        while( left < right){            
             int mid = left + (right - left) /2;  
-            if(isBadVersion(mid) == true) {
-                right = mid;
-            } else {
-                left = mid+1;
-            }
+            if(isBadVersion(mid) == true) right = mid;
+            else left = mid+1;
         }
         return left;
     }
