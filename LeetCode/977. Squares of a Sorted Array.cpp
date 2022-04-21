0eb585a984885fed546267d813c0732fa10a6569
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-21 11:34:21 +0530

    Create 977. Squares of a Sorted Array.cpp

---
+++ LeetCode/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,31 @@
+/*
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        multiset<int> lset;
+        vector<int> sqr(nums.size());        
+        for(int i=0; i<nums.size(); i++){
+            lset.insert(nums[i] * nums[i]);
+        }
+        
+        copy(lset.begin(), lset.end(), sqr.begin());        
+        return sqr;
+    }
+};
