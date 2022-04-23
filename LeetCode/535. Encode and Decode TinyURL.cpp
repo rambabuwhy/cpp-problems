5eff3953907cdd465b84573a47c785c78a8e8b17
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-04-23 13:04:10 +0530

    Update 535. Encode and Decode TinyURL.cpp

--- LeetCode/535. Encode and Decode TinyURL.cpp
+++ LeetCode/535. Encode and Decode TinyURL.cpp
@@ -1,10 +1,26 @@
 /*
 
 Note: This is a companion problem to the System Design problem: Design TinyURL.
-TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
 
-Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
 
+Implement the Solution class:
+
+Solution() Initializes the object of the system.
+String encode(String longUrl) Returns a tiny URL for the given longUrl.
+String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+ 
+
+Example 1:
+
+Input: url = "https://leetcode.com/problems/design-tinyurl"
+Output: "https://leetcode.com/problems/design-tinyurl"
+
+Explanation:
+Solution obj = new Solution();
+string tiny = obj.encode(url); // returns the encoded tiny url.
+string ans = obj.decode(tiny); // returns the original url after deconding it.
 
 */
 
@@ -13,19 +29,18 @@ public:
     
     hash<string> lhash;
     unordered_map<string, string> lmap; 
-
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
         
-        string key = to_string( lhash(longUrl) );
+        string key = to_string(lhash(longUrl));
         lmap[ key ] = longUrl;
-        return key;
-        
+        return key;        
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return lmap[ shortUrl ];
+        return lmap[shortUrl];
     }
 };
 
