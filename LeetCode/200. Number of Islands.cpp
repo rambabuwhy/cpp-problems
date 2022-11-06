a98802a1405e60aed24a9342e9a07b13687827e0
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-11-06 17:01:05 +0530

    Update 200. Number of Islands.cpp

--- LeetCode/200. Number of Islands.cpp
+++ LeetCode/200. Number of Islands.cpp
@@ -29,35 +29,29 @@ Output: 3
 
 class Solution {
 public:
-    void numIslandsUtil(vector<vector<char>>& grid, int r, int c){
-        
+
+    void dfs(vector<vector<char>>& grid, int r, int c){
+
+        if(grid[r][c] == '0') return;
         grid[r][c] = '0';
-        if(r-1 >= 0 && grid[r-1][c] == '1') numIslandsUtil(grid,r-1,c);
-        
-        if(r+1 < grid.size() && grid[r+1][c] == '1') numIslandsUtil(grid,r+1,c);
-        
-        if(c-1 >= 0 && grid[r][c-1] == '1') numIslandsUtil(grid,r,c-1);
-        
-        if(c+1 < grid[0].size() && grid[r][c+1] == '1') numIslandsUtil(grid,r,c+1);
-        
+
+        if(r>0)  dfs(grid, r-1,c);
+        if(c>0)  dfs(grid, r,c-1);
+        if(r<grid.size()-1)  dfs(grid, r+1,c);
+        if(c<grid[0].size()-1) dfs(grid, r,c+1);
     }
     int numIslands(vector<vector<char>>& grid) {
-        int result = 0;
-        if(grid.size() <= 0) return result;
-        
-        int rows = grid.size();
-        int cols = grid[0].size();
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < cols; c++){
-                if(grid[r][c] == '1'){
-                    result ++;
-                    numIslandsUtil(grid, r, c);
-                    
+
+        int count=0;
+        for(int i=0; i<grid.size(); i++){
+            for(int j=0; j<grid[0].size(); j++){
+
+                if (grid[i][j] == '1'){
+                    dfs(grid, i,j);
+                    count++;
                 }
-                
             }
         }
-        return result;
-        
+        return count;
     }
 };
