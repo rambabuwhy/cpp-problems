ab6054825a4318c2f3345e41791aa73309ea56a3
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-11-07 10:22:46 +0530

    Update 392. Is Subsequence.cpp

--- LeetCode/392. Is Subsequence.cpp
+++ LeetCode/392. Is Subsequence.cpp
@@ -1,20 +1,37 @@
 /*
-Given a string s and a string t, check if s is subsequence of t.
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
-Follow up:
-If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+ 
 
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
 */
 
-bool isSubsequence(string s, string t) {
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
 
-        int ll = 0 , lr = s.size();
-        int rl = 0,  rr = t.size();
-        while(ll < lr && rl < rr){
-            if(s[ll]==t[rl]) ll++;
-            rl++;
+        int sl = 0 , sr = s.size();  //pointers for string s 
+        int tl = 0,  tr = t.size();  //pointer for string t
+
+        while(sl < sr && tl < tr){
+
+            //increment sl only if find char in t
+            if(s[sl]==t[tl]) sl++;
+
+            //always increment for string t
+            tl++;
         }
-        return ll == lr;
-}
+
+        //check sl and sr pointer if all character match then subsequence exist
+        return sl == sr;
+    }
+};
