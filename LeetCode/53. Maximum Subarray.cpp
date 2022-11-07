0aa3a68fc332d7813bbd371ad379ff61ec58a322
Rambabu Yerajana <rambabuwhy@gmail.com>
2022-11-07 21:40:47 +0530

    Update 53. Maximum Subarray.cpp

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -23,19 +23,27 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         
-        int max_so_far = INT_MIN;
-        int max_ending_here = 0;
-        for(int i = 0; i < nums.size(); i++){
-            
-            max_ending_here += nums[i]; 
-            if (max_so_far < max_ending_here){
-                max_so_far = max_ending_here;
-            }
-            
-            if (max_ending_here < 0){
-                max_ending_here = 0;
-            }
+        vector<int> dp(nums.size(),0);
+        dp[0]=nums[0];
+        int result=dp[0];
+
+        for(int i=1; i<nums.size(); i++){
+            dp[i]=(dp[i-1]>0?dp[i-1]:0) + nums[i];
+            result=max(result,dp[i]);
         }
-        return max_so_far; 
+        return result;
     }
+    /*
+        int maxSubArray(vector<int>& nums) {
+        
+        int max_ending_here = 0;   // max at given index
+        int max_so_far = INT_MIN;  //result: max of (max_ending_here)
+        for(int i = 0; i < nums.size(); i++){
+            max_ending_here += nums[i]; 
+            if (max_so_far < max_ending_here)  max_so_far = max_ending_here;
+            if (max_ending_here < 0) max_ending_here = 0;
+        }
+        return max_so_far;
+    }
+    */
 };
