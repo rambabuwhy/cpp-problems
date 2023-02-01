91fc99086967cbf0f174f49b5bfbf743bfa4a3e2
Rambabu Yerajana <rambabuwhy@gmail.com>
2023-02-01 10:24:23 +0530

    Create 1071. Greatest Common Divisor of Strings.cpp

---
+++ LeetCode/1071. Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,41 @@
+/*
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+
+*/
+
+class Solution {
+public:
+
+    int gcd_util(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }    
+    }
+
+    string gcdOfStrings(string str1, string str2) {
+        
+        if (str1 + str2 != str2 + str1) return "";
+
+        int gcd_len = gcd_util(str1.size(), str2.size());
+        return str1.substr(0, gcd_len);
+    }
+};
