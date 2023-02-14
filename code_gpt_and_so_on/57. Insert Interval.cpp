d0d9eca76e26dc9ea1966be408231d3da256b655
Rambabu Yerajana <rambabuwhy@gmail.com>
2023-02-14 09:46:13 +0530

    Update 57. Insert Interval.cpp

--- code_gpt_and_so_on/57. Insert Interval.cpp
+++ code_gpt_and_so_on/57. Insert Interval.cpp
@@ -1,5 +1,9 @@
 /*
 
+Author: Rambabu Yerajana
+linkedin: https://www.linkedin.com/in/rambabuyerajana/
+problem: Insert Interval
+------------------------------------------------------------------
 Problem:
 You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
 
