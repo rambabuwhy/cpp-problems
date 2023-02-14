cca3cfe2bdc6330ebe3870377c14c07e7c592a03
Rambabu Yerajana <rambabuwhy@gmail.com>
2023-02-14 09:49:11 +0530

    Update 1470. Shuffle the Array.cpp

--- code_gpt_and_so_on/1470. Shuffle the Array.cpp
+++ code_gpt_and_so_on/1470. Shuffle the Array.cpp
@@ -1,8 +1,11 @@
 /*
+
 Author: Rambabu Yerajana
+linkedin: https://www.linkedin.com/in/rambabuyerajana/
 problem: Shuffle the Array
+
 ------------------------------------------------------------------
-problem:
+description:
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
